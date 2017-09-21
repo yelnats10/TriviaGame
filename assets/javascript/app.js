a043ce04d17959c0b314b0f205c101f86b2598eb
yelnats10 <yelnats10@gmail.com>
2017-09-20 23:00:04 -0400

    logic is done need styling

---
+++ assets/javascript/app.js
@@ -0,0 +1,182 @@
+$(document).ready(function() {
+    console.log("ready!");
+});
+
+window.onload = function() {
+    $("#trivia").html("<button type='button' id='start'>Start</button>");
+}
+
+var i = 0;
+var guess = {
+    pickQuestion: [{
+            question: "How do you know when a Chef is angry?",
+            a1: "When he whips the cream and beats the eggs.",
+            a2: "When he stomps his feet and screams out loud.",
+            a3: "When he breaks dishes.",
+            a4: "When he burns the toast.",
+            answer: "When he whips the cream and beats the eggs.",
+        },
+        {
+            question: "What has hands but can not clap?",
+            a1: "Gloves",
+            a2: "A ship",
+            a3: "T-Rex",
+            a4: "A clock",
+            answer: "A clock",
+        },
+        {
+            question: "You walk into a room with a match, a karosene lamp, a candle, and a fireplace. Which do you light first?",
+            a1: "A match",
+            a2: "A karosene lamp",
+            a3: "A candle",
+            a4: "A fireplace",
+            answer: "A match",
+        },
+        {
+            question: "How many months have 28 days?",
+            a1: "1",
+            a2: "3",
+            a3: "8",
+            a4: "12",
+            answer: "12",
+        },
+        {
+            question: "If there are 3 apples and you take away 2, how many do you have?",
+            a1: "1",
+            a2: "2",
+            a3: "3",
+            a4: "none",
+            answer: "2",
+        },
+    ],
+};
+
+
+var clockRunning = false;
+var number = 30;
+var countDown;
+var wins = 0;
+var loses = 0;
+
+function decrement() {
+    number--;
+    $("#time").html("<h2>Time Remaining: " + number + "</h2>");
+    if (number === 0) {
+        $("h2").remove();
+        stop();
+
+        $("#trivia").html("<p class='question'>Wrong!</p>");
+        $("#trivia").append("<p class='question'>Correct answer is</p>");
+        $("#trivia").append("<p class='question'>" + guess.pickQuestion[i].answer + "</p>");
+        i++;
+        setTimeout(next, 5000);
+
+    }
+}
+
+
+function run() {
+    if (!clockRunning) {
+        countDown = setInterval(decrement, 1000);
+        clockRunning = true;
+    }
+}
+
+function reset(){
+	clockRunning = false;
+	number = 30;
+	i = 0;
+	wins = 0;
+	loses = 0;
+	next();
+}
+
+
+function stop() {
+    clearInterval(countDown);
+    clockRunning = false;
+    number = 30;
+}
+
+function initial() {
+    run();
+    if (i === 5) {
+        $("h2").remove();
+        stop();
+        $("#trivia").html("<p class='question'>game over</p>");
+        $("#trivia").append("<p class='question'>Wins: " + wins +"</p>");
+        $("#trivia").append("<p class='question'>Loses: " + loses +"</p>");
+        setTimeout(reset, 5000);
+
+    } else {
+        $("#trivia").html("<p class='question'>" + guess.pickQuestion[i].question + "</p>");
+        $("#trivia").append("<p class='question'>" + guess.pickQuestion[i].a1 + "</p>");
+        $("#trivia").append("<p class='question'>" + guess.pickQuestion[i].a2 + "</p>");
+        $("#trivia").append("<p class='question'>" + guess.pickQuestion[i].a3 + "</p>");
+        $("#trivia").append("<p class='question'>" + guess.pickQuestion[i].a4 + "</p>");
+    }
+
+}
+
+function next() {
+    initial();
+    $(".question").on("click", function() {
+        var picked = ($(this).text());
+        console.log(picked);
+
+        if (picked === guess.pickQuestion[i].answer) {
+            $("h2").remove();
+            stop();
+
+            $("#trivia").html("<p class='question'>Correct!</p>");
+            i++;
+            wins++;
+            setTimeout(next, 5000);
+
+        } else if (picked !== guess.pickQuestion[i].answer) {
+            $("h2").remove();
+            stop();
+
+            $("#trivia").html("<p class='question'>Wrong!</p>");
+            $("#trivia").append("<p class='question'>Correct answer is</p>");
+            $("#trivia").append("<p class='question'>" + guess.pickQuestion[i].answer + "</p>");
+            i++;
+            loses++;
+            setTimeout(next, 5000);
+        }
+
+    });
+}
+
+
+$("#trivia").on("click", "#start", function() {
+
+    initial();
+
+    $(".question").on("click", function() {
+        var picked = ($(this).text());
+        console.log(picked);
+
+        if (picked === guess.pickQuestion[i].answer) {
+            $("h2").remove();
+            stop();
+
+            $("#trivia").html("<p class='question'>Correct!</p>");
+            i++;
+            wins++;
+            setTimeout(next, 5000);
+        } else if (picked !== guess.pickQuestion[i].answer) {
+            $("h2").remove();
+            stop();
+
+            $("#trivia").html("<p class='question'>Wrong!</p>");
+            $("#trivia").append("<p class='question'>Correct answer is</p>");
+            $("#trivia").append("<p class='question'>" + guess.pickQuestion[i].answer + "</p>");
+            i++;
+            loses++;
+            setTimeout(next, 5000);
+        }
+
+    });
+
+});
